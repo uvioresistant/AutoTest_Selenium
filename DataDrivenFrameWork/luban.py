9c08ff91700c2f4055e4273cb2650eb833b32b8a
uvioresistant <kaixin654321@gmail.com>
2020-08-05 10:41:41 +0800

    修改luban登录

--- DataDrivenFrameWork/luban.py
+++ DataDrivenFrameWork/luban.py
@@ -17,17 +17,17 @@ driver.implicitly_wait(3)
 # 最大化浏览器窗口
 driver.maximize_window()
 # 访问鲁班登录页面
-driver.get("http://128.196.108.10:8080/#/login")
+driver.get("http://xxx.xxx.xxx.xxx:8080/#/login")
 # 暂停3s，等待登录页面加载完成
 time.sleep(3)
 # 获取用户名输入框
 userName = driver.find_element_by_xpath('//*[@id="app"]/div/form/div[2]/div/div/input')
 # 输入用户名
-userName.send_keys("huangwk")
+userName.send_keys("xxx")
 # 获取密码输入框
 pwd = driver.find_element_by_xpath('//*[@id="app"]/div/form/div[3]/div/div/input')
 # 输入密码
-pwd.send_keys("abc123456")
+pwd.send_keys("xxx")
 
 # 单击"域名"按钮
 driver.find_element_by_xpath('//*[@id="app"]/div/form/div[4]/div[1]/div/div[1]/div/input').click()

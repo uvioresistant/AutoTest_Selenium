7e4c8e8d01a9d14284c8902de7a44056305fbb74
uvioresistant <kaixin654321@gmail.com>
2020-08-10 16:53:30 +0800

    修改testScripts包中__init__.py,添加excel解析对象

--- KeyWordAndDataDrivenFrameWork/testScripts/__init__.py
+++ KeyWordAndDataDrivenFrameWork/testScripts/__init__.py
@@ -5,6 +5,13 @@
 @file:__init__.py.py
 @time:2020/08/07
 """
+from action.PageAction import *
+from util.ParseExcel import ParseExcel
+from config.VarConfig import *
+import time
+import traceback
 
-if __name__ == "__main__":
-    pass
+# 创建解析Excel对象
+excelObj = ParseExcel()
+# 将Excel数据文件加载到内存
+excelObj.loadWorkBook(dataFilePath)

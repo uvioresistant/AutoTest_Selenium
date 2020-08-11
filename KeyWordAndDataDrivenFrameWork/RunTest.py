ff193f368ea790f4926df5209396c18524bb42bb
uvioresistant <kaixin654321@gmail.com>
2020-08-11 16:06:01 +0800

    在config包中新建Logger.conf：用于配置日志基本信息;util包新建Log.py：用于初始化日志对象

---
+++ KeyWordAndDataDrivenFrameWork/RunTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:fengg
+@file:RunTest.py
+@time:2020/08/11
+"""
+from testScripts.TestSendMailAndCreateContacts import TestSendMailAndCreateContacts
+
+if __name__ == "__main__":
+    TestSendMailAndCreateContacts()
